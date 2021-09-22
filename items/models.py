4ec62598da1bdc4883b25d43013d76df6887a9ed
post1blues <yura1kovalyov@gmail.com>
2021-09-22 13:46:51 +0300

    Create Item model

--- items/models.py
+++ items/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from users.models import CustomUser
+
 
 class Category(models.Model):
     title = models.CharField(verbose_name='Категория', max_length=50)
@@ -9,3 +11,29 @@ class Category(models.Model):
         verbose_name_plural = 'Категории'
 
     def __str__(self): return self.title
+
+
+class Item(models.Model):
+    title = models.CharField(max_length=100, verbose_name='название')
+    description = models.TextField(blank=True, verbose_name='описание')
+    category = models.ForeignKey(
+        'Category',
+        on_delete=models.CASCADE,
+        verbose_name='категория',
+        related_name='items'
+    )
+    owner = models.ForeignKey(
+        'CustomUser',
+        on_delete=models.CASCADE,
+        verbose_name='владелец',
+        related_name='items'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'предмет на обмен'
+        verbose_name_plural = 'предметы на обмен'
+        ordering = ['-created_at']
