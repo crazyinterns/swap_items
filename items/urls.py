39deca0671da654f776fcdcf8dde0b5d011039f5
Yuriy Belov <paganism@mail.ru>
2021-09-25 09:10:35 +0300

    add card deck & user items page

--- items/urls.py
+++ items/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 
 
-from items.views import index, item_detail, new_item, send_offer, offers
+from items.views import index, item_detail, new_item, send_offer, \
+    offers, user_items
 
 urlpatterns = [
     path('', index, name='index'),
     path('items/<int:pk>', item_detail, name='item_detail'),
     path('new_item/', new_item, name='new_item'),
     path('send_offer/<int:pk>', send_offer, name='send_offer'),
-    path('offers/', offers, name='offers')
+    path('offers/', offers, name='offers'),
+    path('user_items/<int:pk>', user_items, name='user_items'),
 ]
